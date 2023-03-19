6d9601917af29ceb669faa59f05424cd089c7843
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-20 01:38:44 +0800

    :tada: feat: create sign up page

---
+++ src/pages/Landing/SignUp/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpForm from "@/components/Authentication/SignUpForm";
+
+const SignUp = () => {
+  return (
+    <div className="isolate bg-white">
+      <main>
+        <SignUpForm></SignUpForm>
+      </main>
+    </div>
+  );
+};
+
+export default SignUp;
