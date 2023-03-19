f011a5e467ec6b717918d27a30a10b09d82d5dbc
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-20 01:34:06 +0800

    :tada: feat: create AuthService and UserService

---
+++ src/api/Authentication/AuthService.ts
@@ -0,0 +1,86 @@
+import {
+  removeLocalStorageValue,
+  setLocalStorageValue,
+} from "@/utils/miscellaneous";
+import ApiService, { ApiData } from "@/api/ApiService";
+
+export type UserData = {
+  id: number;
+  email: string;
+};
+
+export interface UserSignUpData {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface LoginData {
+  accessToken: string;
+  user: UserData;
+}
+
+export default class AuthService {
+  private static getAuthUrl() {
+    return "";
+  }
+
+  public static async login(
+    email: string,
+    password: string
+  ): Promise<ApiData<LoginData>> {
+    try {
+      //get the token
+      const response = await ApiService.request({
+        url: `${this.getAuthUrl()}/signIn`,
+        method: "POST",
+        data: {
+          email,
+          password,
+        },
+      });
+
+      if (!response || !response.data.accessToken) {
+        //login failed
+        throw new Error("login failed!");
+      }
+
+      // store the x-auth-token in localStorage
+      const accessToken: string = response.data.accessToken;
+      setLocalStorageValue(ApiService.authTokenKey, accessToken);
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async register(
+    userRegisterData: UserSignUpData
+  ): Promise<ApiData> {
+    try {
+      const response = await ApiService.request({
+        url: `${this.getAuthUrl()}/signUp`,
+        method: "POST",
+        data: {
+          ...userRegisterData,
+        },
+      });
+
+      if (!response || !response.data.accessToken) {
+        //login failed
+        throw new Error("signup failed!");
+      }
+
+      // store the x-auth-token in localStorage
+      const accessToken: string = response.data.accessToken;
+      setLocalStorageValue(ApiService.authTokenKey, accessToken);
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static logout(): void {
+    removeLocalStorageValue(ApiService.authTokenKey);
+  }
+}
---
+++ src/api/User/UserService.ts
@@ -0,0 +1,22 @@
+// import ApiService, { ApiData } from '@/api/ApiService';
+
+export default class UserService {
+  private static getUserUrl() {
+    return "users";
+  }
+
+  // public static async getSelf(): Promise<ApiData> {
+  //   try {
+  //     const response = await ApiService.request(
+  //       {
+  //         url: `${this.getUserUrl()}/getSelf`,
+  //         method: 'GET',
+  //       },
+  //       true,
+  //     );
+  //     return response;
+  //   } catch (error) {
+  //     return Promise.reject(error);
+  //   }
+  // }
+}
