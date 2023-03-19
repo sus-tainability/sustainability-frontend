90bdbaa7844588c9c02f6012bb3d567bb7d2fbf8
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-20 00:34:10 +0800

    :bug: fix tailwind config

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+const TailWindTheme = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   important: true,
   theme: {
-    fontFamily: { Inter: ['Inter', 'sans-serif'], epilogue: ['Epilogue', 'sans-serif'] },
-    extend: {
-      colors: {
-        grey: '#f5f5f5',
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
+
+module.exports = TailWindTheme;
