85c28377b46d4fc481abda05e31dfd0fc0533c96
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-20 11:47:46 +0800

    :tada: feat: create EventService

---
+++ src/api/Event/EventService.ts
@@ -0,0 +1,69 @@
+import ApiService, { ApiData } from "@/api/ApiService";
+import { AttemptData } from "@/api/Attempt/AttemptService";
+
+export type EventData = {
+  id: number;
+  name: string;
+  description: string;
+  validationText: string;
+  carbonSavePerAsset: number;
+  eventDuration: number;
+  reward: number;
+  requiredAssets: number;
+  imageUrl: string;
+  activeParticipants: number;
+  createdAt: string;
+  updatedAt: string;
+  attempt: AttemptData;
+};
+
+export default class EventService {
+  private static getEventUrl() {
+    return "events";
+  }
+
+  public static async getEventById(id: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getEventUrl()}/${id}`,
+          method: "GET",
+        },
+        true
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getCurrentEvents(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getEventUrl()}/current`,
+          method: "GET",
+        },
+        true
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getNextEvents(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getEventUrl()}/next`,
+          method: "GET",
+        },
+        true
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
