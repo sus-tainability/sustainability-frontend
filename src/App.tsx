4eb4f191467b30b2ed4d78baad0fc46e90ceb027
ShenyiCui <shenyicui@outlook.com>
2023-03-29 00:12:05 +0800

    :sparkles: feat: add rotate phone to portrait modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { setupIonicReact } from "@ionic/react";
 import { RecoilRoot } from "recoil";
 import BaseRouter from "./components/Routers/BaseRouter";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useState } from "react";
+
+import phoneImg from "./assets/phone.png";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -30,10 +34,37 @@ setupIonicReact();
 
 const App: React.FC = () => {
   const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
+  const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+  const [showSwapOrientation, setShowSwapOrientation] = useState(!isPortrait);
+
+  window
+    .matchMedia("(orientation: portrait)")
+    .addEventListener("change", (e) => {
+      const isPortrait = e.matches;
+      if (isPortrait) {
+        setShowSwapOrientation(false);
+      } else {
+        setShowSwapOrientation(true);
+      }
+    });
 
   return (
     <GoogleOAuthProvider clientId={client_id}>
       <RecoilRoot>
+        {showSwapOrientation && (
+          <div
+            style={{
+              backdropFilter: "blur(8px)",
+              WebkitBackdropFilter: "blur(8px)",
+            }}
+            className="z-30 w-full h-full absolute flex justify-center items-center"
+          >
+            <div className="w-80 h-56 rounded-lg bg-white flex flex-col justify-center items-center font-body">
+              <img className="w-28" src={phoneImg} />
+              <p className="mt-3">Rotate Your Phone to Portrait Mode</p>
+            </div>
+          </div>
+        )}
         <BaseRouter />
         <Toaster />
       </RecoilRoot>
