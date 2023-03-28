c680dc7a998cd62dd8a5b9d1da485acd4228db7b
ShenyiCui <shenyicui@outlook.com>
2023-03-29 00:14:53 +0800

    :bug: fix: eslint error

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,11 +4,11 @@ import ProfileCard from "@/components/ProfileCard";
 import ProfileBadge from "@/components/ProfileBadge";
 
 import profileBgImg from "@/assets/profile_bg.png";
-import { ReactComponent as BackIcon } from "@/assets/profile/backButton.svg"
-import { ReactComponent as TreeIcon } from "@/assets/profile/tree.svg"
-import { ReactComponent as ConsumptionIcon } from "@/assets/profile/consumption.svg"
-import { ReactComponent as ExploreIcon } from "@/assets/profile/currentlyExploring.svg"
-import { ReactComponent as LifeWaterIcon } from "@/assets/profile/lifeBelowWater.svg"
+import { ReactComponent as BackIcon } from "@/assets/profile/backButton.svg";
+import { ReactComponent as TreeIcon } from "@/assets/profile/tree.svg";
+import { ReactComponent as ConsumptionIcon } from "@/assets/profile/consumption.svg";
+import { ReactComponent as ExploreIcon } from "@/assets/profile/currentlyExploring.svg";
+import { ReactComponent as LifeWaterIcon } from "@/assets/profile/lifeBelowWater.svg";
 
 import { useApi } from "@/api/ApiHandler";
 import UserService from "@/api/User/UserService";
@@ -29,13 +29,18 @@ const Profile: React.FC = () => {
       }
     };
     updateUserData();
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <IonPage>
       <IonContent>
         <div className="h-full">
-          <img className="fixed w-full h-full" alt="background" src={profileBgImg} />
+          <img
+            className="fixed w-full h-full"
+            alt="background"
+            src={profileBgImg}
+          />
           <div className="fixed w-full h-full bg-black opacity-60" />
 
           <div className="relative flex flex-col gap-6 p-8">
@@ -52,31 +57,61 @@ const Profile: React.FC = () => {
             <div className="flex flex-col gap-6">
               <ProfileCard title="Your Eco-Credits">
                 <div className="flex items-end gap-2">
-                  <IonText className="text-[27px] leading-[32px] font-bold">{user.totalPoints}</IonText>
-                  <IonText className="text-[16px] leading-[24px]">credits</IonText>
+                  <IonText className="text-[27px] leading-[32px] font-bold">
+                    {user.totalPoints}
+                  </IonText>
+                  <IonText className="text-[16px] leading-[24px]">
+                    credits
+                  </IonText>
                 </div>
               </ProfileCard>
               <ProfileCard title="Your Impact since...">
                 <div className="flex items-end gap-2">
-                  <IonText className="text-[27px] leading-[32px] font-bold">{user.totalCarbonSaved}</IonText>
-                  <IonText className="text-[16px] leading-[24px]">kg CO2e</IonText>
+                  <IonText className="text-[27px] leading-[32px] font-bold">
+                    {user.totalCarbonSaved}
+                  </IonText>
+                  <IonText className="text-[16px] leading-[24px]">
+                    kg CO2e
+                  </IonText>
                 </div>
               </ProfileCard>
               <ProfileCard title="Comparing with others...">
                 <div className="flex flex-col">
-                  <IonText className="text-[27px] leading-[32px] font-bold">Top 15% eco-warriors</IonText>
-                  <IonText className="text-[16px] leading-[24px]">in your age group</IonText>
+                  <IonText className="text-[27px] leading-[32px] font-bold">
+                    Top 15% eco-warriors
+                  </IonText>
+                  <IonText className="text-[16px] leading-[24px]">
+                    in your age group
+                  </IonText>
                 </div>
               </ProfileCard>
             </div>
 
-            <IonText className="text-[21px] text-white">Your Eco-badges</IonText>
+            <IonText className="text-[21px] text-white">
+              Your Eco-badges
+            </IonText>
 
             <div className="flex flex-wrap items-center justify-between gap-y-6">
-              <ProfileBadge title="Life on Land" color="#59BA47" icon={<TreeIcon />} />
-              <ProfileBadge title="Life Below Water" color="#1F97D4" icon={<LifeWaterIcon />} />
-              <ProfileBadge title="Responsible Consumption" color="#BF8D2C" icon={<ConsumptionIcon />} />
-              <ProfileBadge title="Curently Exploring..." color="#7C7278" icon={<ExploreIcon />} />
+              <ProfileBadge
+                title="Life on Land"
+                color="#59BA47"
+                icon={<TreeIcon />}
+              />
+              <ProfileBadge
+                title="Life Below Water"
+                color="#1F97D4"
+                icon={<LifeWaterIcon />}
+              />
+              <ProfileBadge
+                title="Responsible Consumption"
+                color="#BF8D2C"
+                icon={<ConsumptionIcon />}
+              />
+              <ProfileBadge
+                title="Curently Exploring..."
+                color="#7C7278"
+                icon={<ExploreIcon />}
+              />
             </div>
           </div>
         </div>
