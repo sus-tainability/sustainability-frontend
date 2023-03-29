6e5b1dc78cbc1ada2892166ffea96c16e3f7f729
ShenyiCui <shenyicui@outlook.com>
2023-03-29 22:03:59 +0800

    :bug: fix: eslint error

--- src/pages/PhotoLanding/PhotoLanding.tsx
+++ src/pages/PhotoLanding/PhotoLanding.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { IonContent, IonPage, IonText } from "@ionic/react";
 
 import InformationFooter from "@/components/InformationFooter";
 import AppButton from "@/components/AppButton/AppButton";
 
 import photoLandingImg from "@/assets/photoLandingImg.png";
-import { ReactComponent as PhotoPlaceholder } from "@/assets/photoPlaceholder.svg"
+import { ReactComponent as PhotoPlaceholder } from "@/assets/photoPlaceholder.svg";
 
 export const PhotoLanding: React.FC = () => {
   return (
@@ -15,18 +16,23 @@ export const PhotoLanding: React.FC = () => {
           <InformationFooter heightOffSet={300}>
             <div className="flex flex-col gap-5 p-8">
               <IonText className="text-[36px] text-lightShade font-bold">
-                You are <br />almost there!
+                You are <br />
+                almost there!
               </IonText>
 
               <div className="bg-white rounded-xl flex justify-between items-center px-5 py-4">
                 <IonText className="font-bold font-body">
-                  Your<br /> submission
+                  Your
+                  <br /> submission
                 </IonText>
                 <PhotoPlaceholder />
               </div>
 
               <IonText className="font-body text-lightShade text-[16px]">
-                Before submitting your challenge attempt, please help us verify your peers’ submissions! complete a simple community verification. This is ensures that your peers do the right thing!
+                Before submitting your challenge attempt, please help us verify
+                your peers’ submissions! complete a simple community
+                verification. This is ensures that your peers do the right
+                thing!
               </IonText>
 
               <div className="grid grid-cols-2 grid-rows-1 gap-4">
@@ -34,9 +40,7 @@ export const PhotoLanding: React.FC = () => {
                   Back
                 </AppButton>
 
-                <AppButton className="py-5">
-                  Let's Go!
-                </AppButton>
+                <AppButton className="py-5">Let's Go!</AppButton>
               </div>
             </div>
           </InformationFooter>
