72db4781a5def165bd24ccc7870c17540ab5994a
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-30 17:01:48 +0800

    :sparkles: feat: style login page

--- src/components/Authentication/LoginForm/LoginForm.tsx
+++ src/components/Authentication/LoginForm/LoginForm.tsx
@@ -23,75 +23,72 @@ const LoginForm = () => {
   return (
     <IonPage>
       <IonContent>
-        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Log in to your account
-            </h2>
-          </div>
-
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <button
-                    disabled={status === "pending"}
-                    type="submit"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      execute();
-                    }}
-                    className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Log in
-                  </button>
-                </div>
-              </form>
-
-              <SingleSignOn />
+        <div className="flex bg-gradient-to-b from-[#652E20] to-[#332019] min-h-full flex-col justify-center sm:px-6 lg:px-8">
+          <div className="bg-white bg-opacity-20 m-6 rounded-lg shadow p-7">
+            <div className="sm:mx-auto mb-4 sm:w-full sm:max-w-md">
+              <h2 className="text-center text-3xl font-bold tracking-tight text-white">
+                Log in to EcoQuest
+              </h2>
             </div>
+            <form className="space-y-4" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-lg font-medium text-white"
+                >
+                  Email
+                </label>
+                <div className="mt-1">
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-lg font-medium text-white"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  disabled={status === "pending"}
+                  type="submit"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    execute();
+                  }}
+                  className="flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-lg font-semibold text-black shadow-sm bg-white bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                >
+                  Log in
+                </button>
+              </div>
+            </form>
+
+            <SingleSignOn />
           </div>
         </div>
       </IonContent>
--- src/components/Authentication/SingleSignOn.tsx
+++ src/components/Authentication/SingleSignOn.tsx
@@ -56,14 +56,12 @@ const SingleSignOn = () => {
     <div className="mt-4">
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
-          <div className="w-full border-t border-gray-300" />
+          <div className="w-full border-t border-gray-300 border-2 mx-5 rounded-full" />
         </div>
-        <div className="relative flex justify-center text-sm">
-          <span className="bg-white px-2 text-gray-500">Or continue with</span>
-        </div>
+        <div className="relative flex justify-center text-sm"></div>
       </div>
 
-      <div className="mt-3 grid grid-cols-1 gap-3">
+      <div className="mt-4 grid grid-cols-1 gap-3">
         <div>
           <div
             onClick={() => googleLogin()}
@@ -76,10 +74,12 @@ const SingleSignOn = () => {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
+              className="h-5 w-5 self-center"
             >
               {" "}
               <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />{" "}
             </svg>
+            <p className="text-lg ml-3">Continue With Google </p>
           </div>
         </div>
       </div>
