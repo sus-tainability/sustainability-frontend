14a13c7d4d2ab098f13879aa787fdaa07d9d86d1
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-04-01 04:56:31 +0800

    :sparkles: fix: add loading to login page

--- src/components/Authentication/SingleSignOn.tsx
+++ src/components/Authentication/SingleSignOn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import ApiService from "@/api/ApiService";
@@ -8,6 +8,7 @@ import { useRecoilState } from "recoil";
 import { useApi } from "@/api/ApiHandler";
 import UserService from "@/api/User/UserService";
 import { userAtom } from "@/utils/atoms/user";
+import LoadingPage from "@/components/LoadingPage/LoadingPage";
 
 const baseUrl = process.env.REACT_APP_API_URL;
 
@@ -17,6 +18,7 @@ const SingleSignOn = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [__, setUser] = useRecoilState(userAtom);
   const [getSelf] = useApi(() => UserService.getSelf(), false, false, false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const googleLogin = useGoogleLogin({
     flow: "auth-code",
@@ -32,6 +34,7 @@ const SingleSignOn = () => {
           type: ToasterType.ERROR,
         });
       }
+      setIsLoading(true);
 
       // store the x-auth-token in localStorage
       const accessToken: string = response.data.accessToken;
@@ -47,43 +50,49 @@ const SingleSignOn = () => {
         setUser((prev) => ({ ...prev, ...res.data }));
       }
       window.setTimeout(() => {
+        setIsLoading(false);
         window.location.reload();
       }, 1500);
     },
   });
 
   return (
-    <div className="mt-4">
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <div className="w-full border-t border-gray-300 border-2 mx-5 rounded-full" />
+    <>
+      {isLoading && <LoadingPage />}
+      <div className="mt-4">
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-300 border-2 mx-5 rounded-full" />
+          </div>
+          <div className="relative flex justify-center text-sm"></div>
         </div>
-        <div className="relative flex justify-center text-sm"></div>
-      </div>
 
-      <div className="mt-4 grid grid-cols-1 gap-3">
-        <div>
-          <div
-            onClick={() => googleLogin()}
-            className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-          >
-            <span className="sr-only">Sign in with Google</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              className="h-5 w-5 self-center"
+        <div className="mt-4 grid grid-cols-1 gap-3">
+          <div>
+            <div
+              onClick={() => {
+                googleLogin();
+              }}
+              className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
             >
-              {" "}
-              <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />{" "}
-            </svg>
-            <p className="text-lg ml-3">Continue With Google </p>
+              <span className="sr-only">Sign in with Google</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                className="h-5 w-5 self-center"
+              >
+                {" "}
+                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />{" "}
+              </svg>
+              <p className="text-lg ml-3">Continue With Google </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
