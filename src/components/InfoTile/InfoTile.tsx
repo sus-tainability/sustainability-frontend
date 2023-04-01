89f44c1fafa9f50a89dae36fec99970de3056537
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2023-04-02 04:07:55 +0800

    fix: Fix InfoTile overlay not appearing on some browsers

--- src/components/InfoTile/InfoTile.tsx
+++ src/components/InfoTile/InfoTile.tsx
@@ -10,9 +10,9 @@ const InfoTile = ({
   text: string;
 }) => {
   return (
-    <div className="h-36 w-36 rounded-lg mr-2">
-      <a className="relative" href={link}>
-        <div className="bg-[#ffffffcd] py-2 rounded-lg font-body font-bold w-full">
+    <div className="relative h-36 w-36 rounded-lg mr-2">
+      <a href={link}>
+        <div className="absolute bottom-0 bg-[#ffffffcd] py-2 rounded-lg font-body font-bold w-full">
           <p className="text-center w-full">{text}</p>
         </div>
         <img
