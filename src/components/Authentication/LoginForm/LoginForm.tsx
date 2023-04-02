163c36314f4af1e758af859c03dcd7f7f8d0ac29
ShenyiCui <shenyicui@outlook.com>
2023-04-03 02:14:58 +0800

    :sparkles: feat: add login form loading state

--- src/components/Authentication/LoginForm/LoginForm.tsx
+++ src/components/Authentication/LoginForm/LoginForm.tsx
@@ -6,12 +6,18 @@ import SingleSignOn from "@components/Authentication/SingleSignOn";
 import useAsync from "@/hooks/useAsync";
 import { useApi } from "@/api/ApiHandler";
 import AuthService from "@/api/Authentication/AuthService";
+import LoadingPage from "@/components/LoadingPage/LoadingPage";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("user1@example.com");
   const [password, setPassword] = useState("asdasd");
 
-  const [login] = useApi(() => AuthService.login(email, password), true, true);
+  const [login] = useApi(
+    () => AuthService.login(email, password),
+    true,
+    true,
+    false
+  );
   const { execute, status, value } = useAsync(login, false);
 
   useEffect(() => {
@@ -21,78 +27,82 @@ const LoginForm = () => {
   }, [status, value]);
 
   return (
-    <IonPage>
-      <IonContent>
-        <div className="flex bg-gradient-to-b from-[#652E20] to-[#332019] min-h-full flex-col justify-center sm:px-6 lg:px-8">
-          <div className="bg-white bg-opacity-20 m-6 rounded-lg shadow p-7">
-            <div className="sm:mx-auto mb-4 sm:w-full sm:max-w-md">
-              <h2 className="text-center text-3xl font-bold tracking-tight text-white">
-                Log in to EcoQuest
-              </h2>
+    <>
+      {status === "pending" && <LoadingPage />}
+      {status !== "pending" && (
+        <IonPage>
+          <IonContent>
+            <div className="flex bg-gradient-to-b from-[#652E20] to-[#332019] min-h-full flex-col justify-center sm:px-6 lg:px-8">
+              <div className="bg-white bg-opacity-20 m-6 rounded-lg shadow p-7">
+                <div className="sm:mx-auto mb-4 sm:w-full sm:max-w-md">
+                  <h2 className="text-center text-3xl font-bold tracking-tight text-white">
+                    Log in to EcoQuest
+                  </h2>
+                </div>
+                <form className="space-y-4" action="#" method="POST">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-lg font-medium text-white"
+                    >
+                      Email
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-lg font-medium text-white"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none :text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        execute();
+                      }}
+                      className="flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-lg font-semibold text-black shadow-sm bg-white bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                    >
+                      Log in
+                    </button>
+                  </div>
+                </form>
+
+                <SingleSignOn />
+              </div>
             </div>
-            <form className="space-y-4" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-lg font-medium text-white"
-                >
-                  Email
-                </label>
-                <div className="mt-1">
-                  <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-lg font-medium text-white"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none :text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  disabled={status === "pending"}
-                  type="submit"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    execute();
-                  }}
-                  className="flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-lg font-semibold text-black shadow-sm bg-white bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                >
-                  Log in
-                </button>
-              </div>
-            </form>
-
-            <SingleSignOn />
-          </div>
-        </div>
-      </IonContent>
-    </IonPage>
+          </IonContent>
+        </IonPage>
+      )}
+    </>
   );
 };
 
